061abc1ce2bd46d8012143c3cba07c4d02b6dc75
Andrew Nero <andrew.nero@gmail.com>
2018-06-25 17:17:08 -0400

    color

--- lib/ui/home_page/order_structure_page.dart
+++ lib/ui/home_page/order_structure_page.dart
@@ -69,6 +69,9 @@ class OrderStructurePage extends StatelessWidget {
                                    Item.getItemFromDocId(selection.itemDocId).name, style: Theme.of(context).textTheme.title,)),
                             Expanded(child:
                             new Wrap(
+
+                              spacing: 3.0,
+                              runSpacing: 2.0,
                               children: _getList(selection.choices.values)
                                   .map<Widget>((String choice) {
                                 return new ChoiceChip(
